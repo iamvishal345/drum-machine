55a7ad2c5bb0cbac3f7e47e043908ed657602f18
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-14 21:05:02 +0530

    Updates

---
+++ precache-manifest.e59f74e6a7c01377f23fe176962ac0fc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c59b7fe48b078acb5aef2cd8a376f13b",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "506f7536c040b6dbe2c6",
+    "url": "/drum-machine/static/css/main.e4a23237.chunk.css"
+  },
+  {
+    "revision": "8092dcf56064c8dbe357",
+    "url": "/drum-machine/static/js/2.88f700bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.88f700bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506f7536c040b6dbe2c6",
+    "url": "/drum-machine/static/js/main.a393f38a.chunk.js"
+  },
+  {
+    "revision": "d7d1943964fcc80e9287",
+    "url": "/drum-machine/static/js/runtime-main.49a0ba3c.js"
+  },
+  {
+    "revision": "39f9581affa38e7f9322157d85e1ff57",
+    "url": "/drum-machine/static/media/clap.39f9581a.wav"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/drum-machine/static/media/kick.550be808.wav"
+  },
+  {
+    "revision": "dede7ee99879684773eb9f3b6eb91552",
+    "url": "/drum-machine/static/media/kick_zapper.dede7ee9.wav"
+  },
+  {
+    "revision": "a62db465466a58a60a40c4466d7730e5",
+    "url": "/drum-machine/static/media/openhat_acoustic.a62db465.wav"
+  },
+  {
+    "revision": "b0d70ec54db9b098b9358f98d131c0f9",
+    "url": "/drum-machine/static/media/ride_acoustic.b0d70ec5.wav"
+  },
+  {
+    "revision": "ffebfc9e6d21e7f8798c4973d940377e",
+    "url": "/drum-machine/static/media/snare.ffebfc9e.wav"
+  },
+  {
+    "revision": "5e055a5dc4a7a699d41b3357ee096149",
+    "url": "/drum-machine/static/media/snare_analog.5e055a5d.wav"
+  },
+  {
+    "revision": "983e56d97cfa4cb91af97c7f629bac27",
+    "url": "/drum-machine/static/media/snare_modular.983e56d9.wav"
+  },
+  {
+    "revision": "79c2c9b6814b574a78866d4efa8467c8",
+    "url": "/drum-machine/static/media/tom_fm.79c2c9b6.wav"
+  }
+]);
