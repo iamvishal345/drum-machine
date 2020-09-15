503b0c88c9a9ef7c1b98d7c6a45b4b3b44a84dd4
iamvishal345 <vishalagnihotri345@gmail.com>
2020-09-15 14:18:42 +0530

    Minor fix to increase response time

--- src/components/drum-pad.jsx
+++ src/components/drum-pad.jsx
@@ -25,18 +25,14 @@ const DrumPad = ({ name, value, handleClick, drumStatus, volume }) => {
   let clip_ref = useRef(null);
   const [pressed, setPressed] = useState(false);
   const playAudio = useCallback(() => {
-    if (drumStatus) {
-      clip_ref.current.volume = volume / 100;
-      clip_ref.current.currentTime = 0;
-      clip_ref.current.play().then(() => {
-        setPressed(true);
-      });
-      handleClick(value.replace(/_/g, " ").toUpperCase());
-      setTimeout(() => {
-        setPressed(false);
-      }, 100);
-    }
-  }, [drumStatus, handleClick, value, volume]);
+    clip_ref.current.volume = volume / 100;
+    clip_ref.current.currentTime = 0;
+    setPressed(true);
+    clip_ref.current.play().then(() => {
+      setPressed(false);
+    });
+    handleClick(value.replace(/_/g, " ").toUpperCase());
+  }, [handleClick, value, volume]);
   const handleLocalClick = () => {
     if (drumStatus) {
       playAudio();
